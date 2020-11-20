89cd56b653a0757875f67a6a4b4450f11f3c5993
ABerggren <alexander.berggren365@gmail.com>
2020-11-20 07:46:57 +0100

    Added TokenAuthenticatorTest

---
+++ SpotifyMyAPI.Web.Tests/Http/TokenAuthenticatorTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using SpotifyMyAPI.Web;
+using SpotifyMyAPI.Web.Http;
+
+namespace SpotifyAPI.Web.Tests
+{
+    [TestFixture]
+    public class TokenAuthenticatorTest
+    {
+        [Test]
+        public void Apply_AddsCorrectHeader()
+        {
+            var authenticator = new TokenAuthenticator("MyToken", "Bearer");
+            var request = new Mock<IRequest>();
+            var apiConnector = new Mock<IAPIConnector>();
+
+            request.SetupGet(r => r.Headers).Returns(new Dictionary<string, string>());
+
+            authenticator.Apply(request.Object, apiConnector.Object);
+            Assert.AreEqual(request.Object.Headers["Authorization"], "Bearer MyToken");
+        }
+    }
+}
